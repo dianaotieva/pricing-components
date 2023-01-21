382462d84ae892ecebfe6d98fd7d327c55c8ff03
Diana <100135242+dianaotieva@users.noreply.github.com>
2023-01-21 15:10:05 +0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 let mainContent = document.querySelector('.pr');
-let secondaryContent = document.querySelector('.pri-after');
+let secondaryContent = document.querySelector('.pr-after');
 
-document.querySelector('.switch input').addEventListener('change', e => { 
-  mainContent.style.display = e.target.checked ? 'block' : 'none';
-  secondaryContent.style.display = e.target.checked ? 'none' : 'block';
+document.querySelector('#switch').addEventListener('change', e => { 
+  mainContent.style.display = e.target.checked ? 'none' : 'block';
+  secondaryContent.style.display = e.target.checked ? 'block' : 'none' ;
 });
-
