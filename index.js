231b63461d4bdf7b829a723bb5003ed4cd42a187
Diana <100135242+dianaotieva@users.noreply.github.com>
2023-01-23 16:39:49 +0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-let mainContent = document.querySelector('.pr');
-let secondaryContent = document.querySelector('.pr-after');
+let priceBasic = document.querySelector('#price-bas');
+let pricePro = document.querySelector('#price-pro')
+let priceMas = document.querySelector('#price-mas')
+let afterBas = document.querySelector('#after-bas');
+let afterPro = document.querySelector('#after-pro');
+let afterMas = document.querySelector('#after-mas')
 
 document.querySelector('#switch').addEventListener('change', e => { 
-  mainContent.style.display = e.target.checked ? 'none' : 'block';
-  secondaryContent.style.display = e.target.checked ? 'block' : 'none' ;
+  priceBasic.style.display = e.target.checked ? 'none' : 'block';
+  afterBas.style.display = e.target.checked ? 'block' : 'none' ;
+
+  pricePro.style.display = e.target.checked ? 'none' : 'block';
+  afterPro.style.display = e.target.checked ? 'block' : 'none' ;
+
+  priceMas.style.display = e.target.checked ? 'none' : 'block';
+  afterMas.style.display = e.target.checked ? 'block' : 'none' ;
+
 });
